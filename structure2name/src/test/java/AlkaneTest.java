07fb20612b17004a527c31b587b0f4e3f1806827
Aleksandr_Savelev <Aleksandr_Savelev@epbyminw0825.minsk.epam.com>
2016-06-06 19:19:26 +0300

    Correction

--- structure2name/src/test/java/AlkaneTest.java
+++ structure2name/src/test/java/AlkaneTest.java
@@ -9,7 +9,7 @@ import com.epam.indigo.IndigoObject;
 import com.epam.structure2name.Namer;
 import java.io.IOException;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 
 /**
@@ -19,16 +19,17 @@ import org.junit.Before;
 public class AlkaneTest {
     Indigo indigo;
    
-   @Before
-   public void setUp() {
-      indigo = new Indigo();
-   }
+    @Before
+    public void setUp() {
+        indigo = new Indigo();
+    }
    
-   @Test
-   public void testBasicGetName() throws IOException {
+    @Test
+    public void testBasicGetName() throws IOException {
         IndigoObject mol1 = indigo.loadMolecule("CCCCCC");
-        assertEquals("hexane", Namer.structure2name(mol1));
-   }
+        assertEquals("n-hexane", Namer.structure2name(mol1));
+    }
+    
     public AlkaneTest() {
     }
     
